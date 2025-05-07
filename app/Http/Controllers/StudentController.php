126f121c2f0928994ca37ab024f845b356042dde
irenecual <cualterosirene@gmail.com>
2025-05-07 10:43:03 +0800

    first commit

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -7,32 +7,33 @@ use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    public function index() {
-        return Student::all();
-    }
-    
-    public function store(Request $request) {
-        $request->validate([
-            'name' => 'required',
-            'email' => 'required|email|unique:students',
-            'course' => 'required',
-        ]);
-        return Student::create($request->all());
-    }
-    
-    public function show(Student $student) {
-        return $student;
-    }
-    
-    public function update(Request $request, Student $student) {
-        $student->update($request->all());
-        return $student;
-    }
-    
-    public function destroy(Student $student) {
-        $student->delete();
-        return response()->json(['message' => 'Deleted successfully']);
-    }
+   
+public function index() {
+    return Student::all();
+}
+
+public function store(Request $request) {
+    $request->validate([
+        'name' => 'required',
+        'email' => 'required|email|unique:students',
+        'course' => 'required',
+    ]);
+    return Student::create($request->all());
+}
+
+public function show(Student $student) {
+    return $student;
+}
+
+public function update(Request $request, Student $student) {
+    $student->update($request->all());
+    return $student;
+}
+
+public function destroy(Student $student) {
+    $student->delete();
+    return response()->json(['message' => 'Deleted successfully']);
+}
     public function create()
 {
     return view('students.create');
